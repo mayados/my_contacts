f4beb88ab61717e45f81204bd73b8ec4fcefd469
mayados <mariannedos.cours@gmail.com>
2025-02-05 21:48:43 +0100

    add relation user to Contact entity

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -25,6 +25,10 @@ class Contact
     #[ORM\Column(length: 20, nullable: true)]
     private ?string $phone = null;
 
+    #[ORM\ManyToOne(inversedBy: 'contacts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -74,6 +78,18 @@ class Contact
     public function setPhone(?string $phone): static
     {
         $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
